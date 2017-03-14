f8b5ec241b3fe109fd8ffdf7b44ec1eb9ea62303
S.Strizhov <beowolfrus@gmail.com>
2017-03-14 21:27:47 +0400

    Added basic game logic

--- Assets/Scripts/BlockLogic.cs
+++ Assets/Scripts/BlockLogic.cs
@@ -4,13 +4,75 @@ using UnityEngine;
 
 public class BlockLogic : MonoBehaviour {
 
+	float fall =0;
+	public float fallSpeed = 1;
+
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		CheckForInput();
+	}
+
+	void CheckForInput()
+	{
+
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			transform.position += new Vector3(1,0,0);
+
+			if(ValidPos())
+			{
+				
+			}
+			else transform.position += new Vector3(-1,0,0);
+		}
+
+		else if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			transform.position += new Vector3(-1,0,0);
+
+			if(ValidPos())
+			{
+
+			}
+			else transform.position += new Vector3(1,0,0);
+		}
+
+		else if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			transform.Rotate (0,0,90);
+		}
+
+		else if((Input.GetKeyDown(KeyCode.DownArrow)) || (Time.time - fall >= fallSpeed))
+		{
+			transform.position += new Vector3(0,-1,0);
+
+			if(ValidPos())
+			{
+
+			}
+			else transform.position += new Vector3(0,1,0);
+
+			fall = Time.time;
+		}
+	}
+
+	bool ValidPos()
+	{
+		foreach(Transform block in transform)
+		{
+			Vector2 pos = FindObjectOfType<GameLogic>().Round (block.position);
+			if(FindObjectOfType<GameLogic>().IsInTheGrid(pos) == false)
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour {
 
+	public int Width = 10;
+	public int Height = 20;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
+	}
+
+	public bool IsInTheGrid(Vector2 pos)
+	{
+		return ((int)pos.x >= 0 &&  (int)pos.x < Width && (int)pos.y >= 0);
+	}
+
+	public Vector2 Round (Vector2 pos)
+	{
+		return new Vector2(Mathf.Round(pos.x),Mathf.Round(pos.y));
 	}
 }
